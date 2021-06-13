3a282d7ba13f1bece4ffc7d6f04a108dc1018f73
Ira <ira.remiracle@gmail.com>
2021-06-13 16:30:17 +0700

    ira

---
+++ app/src/main/java/com/example/projekpemmob/activity/HistoryDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.projekpemmob.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projekpemmob.R;
+
+public class HistoryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_details);
+    }
+}
